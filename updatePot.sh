24cf86ee4eba5526bd709d8e992e6588f5835287
Joshua Strobl <joshua@streambits.io>
2019-08-09 17:49:26 +0300

    Migrate translations to https://translate.getsol.us
    Solus' Weblate is the official replacement for our Transifex and has already been used by Budgie Desktop to facilitate translations. This commit separates out the po files into the dedicated brisk-menu-translations repository and the respective Weblate project+component.

---
+++ updatePot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function do_gettext()
+{
+    xgettext --package-name=brisk-menu --package-version=0.6.0 $* --default-domain=brisk-menu --join-existing --from-code=UTF-8 --no-wrap --keyword=_
+}
+
+function do_intltool()
+{
+    intltool-extract --type=$1 $2
+}
+
+rm brisk-menu.po -f
+touch brisk-menu.po
+
+for file in `find src -name "*.c"`; do
+    do_gettext $file --add-comments --language=C
+done
+
+for file in `find src -name "*.ui"`; do
+    if [[ `grep -F "translatable=\"yes\"" $file` ]]; then
+        do_intltool gettext/glade $file
+        do_gettext ${file}.h --add-comments --keyword=N_:1
+        rm $file.h
+    fi
+done
+
+for file in `find src -name "*.in"`; do
+    if [[ `grep -E "^_*" $file` ]]; then
+        do_intltool gettext/keys $file
+        do_gettext ${file}.h --add-comments --keyword=N_:1
+        rm $file.h
+    fi
+done
+
+mv brisk-menu.po ../brisk-menu-translations/brisk-menu.pot
