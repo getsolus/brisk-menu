c54dcbf9f06c2dc7864dc673f5b67a058cbe4916
Martin Wimpress <code@flexion.org>
2019-08-09 10:17:32 +0100

    mkrelease: Dynamic and portable.
      * Make interpreter path portable.
      * Derive the version from `meson.build`.
      * Use `git archive` directly.
      * White space cleanup.

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Script for ikey because he went with meson. *shrug*
-VERSION="v0.5.0"
+VERSION=v$(grep "version:" meson.build | head -n1 | cut -d"'" -f2)
 NAME="brisk-menu"
-git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+git archive --format tar --prefix ${NAME}-${VERSION}/ --verbose HEAD --output ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
 
 gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
-gpg --verify "${NAME}-${VERSION}.tar.xz.asc"               
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"
